dc9fc9c8e3d620de3dc120ffc0d139ac111acfbd
emma <emmafangsc@gmail.com>
2023-05-25 12:59:52 +0200

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,31 +9,31 @@ const Footer = () => {
                 <div className="menu leading-[3rem]">
                     <h2 className="font-[800] text-[18px] md:text-[20px]">Menu</h2>
                     <ul className="font-[400] text-[14px] md:text-[16px] list-disc">
-                        <li><a>Home Xavier Figueras</a></li>
-                        <li><a>Company</a></li>
-                        <li><a>Individual</a></li>
-                        <li><a>About me</a></li>
-                        <li><a>Blog</a></li>
+                        <li className="cursor-pointer"><a>Home Xavier Figueras</a></li>
+                        <li className="cursor-pointer"><a>Company</a></li>
+                        <li className="cursor-pointer"><a>Individual</a></li>
+                        <li className="cursor-pointer"><a>About me</a></li>
+                        <li className="cursor-pointer"><a>Blog</a></li>
                    </ul>
                 </div>
                 <div className="last-post leading-[3rem]">
                     <h2 className="font-[800] text-[18px] md:text-[20px]">Last Post</h2>
                     <ul className="font-[400] text-[14px] md:text-[16px] list-disc">
-                        <li><a>Healing Process</a></li>
-                        <li><a>Making Your Way</a></li>
-                        <li><a>Individual</a></li>
-                        <li><a>The Personalities</a></li>
-                        <li><a>Hello</a></li>
+                        <li className="cursor-pointer"><a>Healing Process</a></li>
+                        <li className="cursor-pointer"><a>Making Your Way</a></li>
+                        <li className="cursor-pointer"><a>Individual</a></li>
+                        <li className="cursor-pointer"><a>The Personalities</a></li>
+                        <li className="cursor-pointer"><a>Hello</a></li>
                     </ul>
                 </div>
                 <div className="contact-me leading-[3rem]">
                     <h2 className="font-[800] text-[18px] md:text-[20px]">Contact Me</h2>
                     <ul className="font-[400] text-[14px] md:text-[16px] list-disc">
-                        <li><a>Write A Message</a></li>
-                        <li><a>Wahtsapp</a></li>
-                        <li><a>Email</a></li>
-                        <li><a>Policy</a></li>
-                        <li><a>Cookies</a></li>
+                        <li className="cursor-pointer"><a>Write A Message</a></li>
+                        <li className="cursor-pointer"><a>Wahtsapp</a></li>
+                        <li className="cursor-pointer"><a>Email</a></li>
+                        <li className="cursor-pointer"><a>Policy</a></li>
+                        <li className="cursor-pointer"><a>Cookies</a></li>
                     </ul>
                 </div>
             </div>
