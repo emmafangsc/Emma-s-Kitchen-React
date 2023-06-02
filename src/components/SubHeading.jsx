3f511ee70e7368e201bed9d766a644559a628414
emma <emmafangsc@gmail.com>
2023-06-02 08:27:42 +0200

    updated

--- src/components/SubHeading.jsx
+++ src/components/SubHeading.jsx
@@ -4,7 +4,7 @@ const SubHeading = ({title}) => {
     return (
         <div>
             <h1 className="font-OrkneyBold text-[18px] md:text-[36px] leading-[4rem]">{title}</h1>
-            <div className="line pb-[2rem]">
+            <div className="line pb-[1.5rem]">
                 <img src={images.union} width="550px" alt="union"/>
             </div>
         </div>
