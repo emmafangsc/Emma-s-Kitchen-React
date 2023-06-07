6adf6774ebb2136725f2d877ce9e39f484a2ed04
emma <emmafangsc@gmail.com>
2023-06-07 11:14:08 +0200

    column format on ipad

--- src/constants/style.js
+++ src/constants/style.js
@@ -1,5 +1,5 @@
 const styles = {
-    sectionButton: "py-[5px] lg:py-[5px] px-[15px] lg:px-[40px] rounded-lg shadow-lg font-[600] text-[18px] lg:text-[24px]",
+    sectionButton: "py-[5px] md:py-[5px] px-[15px] md:px-[40px] rounded-lg shadow-lg font-[600] text-[18px] md:text-[24px]",
 }
 
 export default styles;
