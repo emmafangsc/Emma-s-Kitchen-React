7eaf95f5760a628c42016405963f0fe05890a172
emma <emmafangsc@gmail.com>
2023-08-08 15:11:19 +0200

    aligned the background for each section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,12 +4,12 @@ import styles from "../constants/style";
 const Section = ({title, content, image}) => {
     return (
 
-        <div className="wrapper px-[2rem] md:px-[4rem] lg:px-[12rem]">
-            <div className="section flex flex-col md:flex-row pb-24  md:gap-[7rem] justify-center">
-                <div className="section__image flex md:w-[25rem] h-[16rem] md:h-auto md:items-start">
+        <div className="wrapper px-[2rem] md:px-[4rem] lg:px-[14rem]">
+            <div className="section flex flex-col md:flex-row pb-40  md:gap-[8rem] justify-center">
+                <div className="section__image flex md:w-[20rem] h-[16rem] md:h-auto md:items-start">
                     <img src={image} alt="section" className="rounded-xl min-w-full object-cover"/>
                 </div>
-                <div className="section__content md:w-[45rem]">
+                <div className="section__content md:w-[40rem]">
                     <SubHeading title={title} />
                     <div className="text font-[400] text-[14px] leading-normal md:text-[18px] lg:leading-relaxed text-justify indent-12 font-OrkneyLight">{content}</div>
                     <div className="button flex justify-end pt-8">
--- src/containers/Certification.jsx
+++ src/containers/Certification.jsx
@@ -2,10 +2,10 @@ import images from "../constants/images";
 const Certification = () => {
     return (
         <div className="wrapper lg:px-[12rem] flex justify-center">
-            <div className="certification flex pb-24 w-full md:w-3/5 px-8 justify-between">
-                    <img src={images.X} className="w-[100px] md:w-[150px]"/>
-                    <img src={images.X} className="w-[100px] md:w-[150px]"/>
-                    <img src={images.X} className="w-[100px] md:w-[150px]" />
+            <div className="certification flex pb-24 w-full md:w-3/5 justify-between mt-[-1.5rem]">
+                    <img src={images.X} className="w-[100px] md:w-[8rem]"/>
+                    <img src={images.X} className="w-[100px] md:w-[8rem]"/>
+                    <img src={images.X} className="w-[100px] md:w-[8rem]" />
                 </div>
         </div>
     )
