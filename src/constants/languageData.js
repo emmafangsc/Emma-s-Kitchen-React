252acbf3c7cc8e7487cfb4037efbc8c7d601ff96
emma <emmafangsc@gmail.com>
2023-08-25 13:18:37 +0200

    Made language button functional

---
+++ src/constants/languageData.js
@@ -0,0 +1,87 @@
+const home = {
+    english: "Home",
+    spanish: "Inicio",
+    vietnamese: "trang chủ",
+    catalan: "casa"
+}
+const company = {
+    english: "company",
+    spanish: "compañía",
+    vietnamese: "công ty",
+    catalan: "empresa"
+}
+const individuals = {
+    english: "individuals",
+    spanish: "individuos",
+    vietnamese: "cá nhân",
+    catalan: "individus"
+}
+const courses = {
+    english: "Courses",
+    spanish: "Cursos",
+    vietnamese: "Khóa học",
+    catalan: "Cursos"
+}
+const aboutme = {
+    english: "About me",
+    spanish: "Acerca de mí",
+    vietnamese: "Về tôi",
+    catalan: "Sobre mi"
+}
+const blog = {
+    english: "Blog",
+    spanish: "Blog",
+    vietnamese: "Blog",
+    catalan: "Bloc"
+}
+const signup = {
+    english: "Sign Up",
+    spanish: "inscribirse",
+    vietnamese: "đăng ký",
+    catalan: "Registra't"
+}
+const english = {
+    english: "ENGLISH",
+    spanish: "INGLÉS",
+    vietnamese: "TIẾNG ANH",
+    catalan: "ANGLÈS't"
+}
+const spanish = {
+    english: "SPANISH",
+    spanish: "ESPAÑOL",
+    vietnamese: "NGƯỜI TÂY BAN NHA",
+    catalan: "ESPANYOL"
+}
+const vietnamese = {
+    english: "VIETNAMESE",
+    spanish: "VIETNAMITA",
+    vietnamese: "TIẾNG VIỆT",
+    catalan: "VIETNAM"
+}
+const catalan = {
+    english: "CATALAN",
+    spanish: "CATALÁN",
+    vietnamese: "CATALAN",
+    catalan: "CATALÀ"
+}
+const languagebutton = {
+    english: "EN",
+    spanish: "ES",
+    vietnamese: "VN",
+    catalan: "CA"
+}
+export const translations = {
+    home,
+    company,
+    individuals,
+    courses,
+    aboutme,
+    blog,
+    signup,
+    english,
+    spanish,
+    vietnamese,
+    catalan,
+    languagebutton
+  };
+  
