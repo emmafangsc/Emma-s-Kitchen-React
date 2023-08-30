51aa57249dbd52b700b6bcc95b3997ed295092b8
emma <emmafangsc@gmail.com>
2023-08-30 10:49:24 +0200

    updated sections magins from mt-64 to mb-32

--- src/containers/HomeSection.jsx
+++ src/containers/HomeSection.jsx
@@ -4,17 +4,17 @@ import Section from "../components/Section";
 const HomeSection = () => {
     return (
         <div>
-            <div>
+            <div className="mb-32">
                 <Section  title={sections.section1.title1} content={sections.section1.content1} image={sections.section1.image1} />
             </div>
-            <div className="section2">
+            <div className="section2 mb-32">
                 <img src={images.headerBG} alt="headerBG"/>
                 <div className="bg-yellow">
                     <Section  title={sections.section2.title2} content={sections.section2.content2} image={sections.section2.image2} />
                 </div>
                 <img src={images.footerBG} alt="footerBG"/>
             </div>
-            <div className="section3">
+            <div className="section3 ">
                 <Section  title={sections.section3.title3} content={sections.section3.content3} image={sections.section3.image3} />
             </div>
         </div>
--- src/containers/Testimonial.jsx
+++ src/containers/Testimonial.jsx
@@ -3,7 +3,7 @@ import Section from "../components/Section";
 import { sections } from "../constants/data";
 const Testimonial = () => {
     return (
-        <div className="testimonials">
+        <div className="testimonials md:mb-32">
             <img src={images.headerBG} alt="headerBG"/>
             <div className="bg-yellow">
                 <Section  title={sections.testimonials.title} content={sections.testimonials.content} image={sections.testimonials.image} />
