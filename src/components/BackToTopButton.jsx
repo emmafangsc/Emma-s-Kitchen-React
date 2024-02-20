9ea60a146d1e86038a71c199eda562195e52f4b5
emma <emmafangsc@gmail.com>
2024-02-20 14:06:20 +0100

    Changed the scroll-up button

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import images from "../constants/images";
 
 const BackToTopButton = () => {
     const [backToTopButton, setBackToTopButton] = useState(false);
@@ -25,7 +24,12 @@ const BackToTopButton = () => {
         <div>
             {backToTopButton && (
             <div onClick={scrollUp} className="scrollUp flex self-end mt-4 mb-16 cursor-pointer fixed z-30 -bottom-12 right-0 sm:right-6">
-                <button><img src={images.scrollUp} alt="scrollUp Button"/></button>
+                <button>
+                    <div className="w-[42px] h-[48px] bg-yellow border-[1px] border-[black] border-b-[5px] flex flex-col items-center justify-center">
+                            <div className="block w-[18px] h-[18px]  border-t-[3px] border-l-[3px] border-black transform rotate-45 translate-y-1/2 mt-[2px]"></div>
+                            <div className="block w-[18px] h-[18px]  border-t-[3px] border-l-[3px] border-black opacity-60 transform rotate-45 mt-[3px]"></div>
+                    </div>
+                </button>
             </div>
         )}
         </div>
